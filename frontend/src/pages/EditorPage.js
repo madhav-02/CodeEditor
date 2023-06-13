3951c2bd214fd948be2d6af0eca065fe9f51e394
Madhav007 <madhavaprashath669@gmail.com>
2023-06-13 21:02:28 +0530

    Update left side bar

--- frontend/src/pages/EditorPage.js
+++ frontend/src/pages/EditorPage.js
@@ -1,9 +1,33 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Client} from '../components/Client';
+import Editor from '../components/Editor';
 function EditorPage() {
-  return (
-    <div>EditorPage</div>
-  )
+    const [clients, setClients] = useState([
+        {socketId: 1, username: 'Madhav'},
+        {socketId: 2, username: 'Prashath'},
+        {socketId: 3, username: 'Django'}
+    ]);
+  return <div className="mainWrapper">
+    <div className="aside">
+        <div className="asideInner">
+            <div className="logo">
+                <img className='logoImage' src="/code-sync.png" alt="logo" />
+            </div>
+            <h3>Connected</h3>
+            <div className="clientsList">
+                {
+                    clients.map( 
+                        (client) => (<Client key={client.socketId} username={client.username}/> )
+            )}
+            </div>
+        </div>
+        <button className='btn copyBtn'>Copy Room ID</button>
+        <button className='btn leaveBtn'>Leave</button>
+    </div>
+    <div className="editorWrap">
+        <Editor />
+    </div>
+  </div>
 }
 
 export default EditorPage
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'; 
 import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 function Home() {
-
+    const navigate = useNavigate();
     const [roomId, setRoomId] = useState('')
     const [username, setUsername] = useState('')
     const createNewRoom = (event) => {
@@ -18,16 +19,27 @@ function Home() {
             toast.error("RoomId and Username is required!");
             return;
         }
-        
+        // If both entered, navigate to the room
+        navigate(`editor/${roomId}`, {
+            state: {
+                username,
+            },
+        })
     } 
+
+    const handleInputEnter = (event) => {
+        if(event.code === 'Enter') {
+            joinRoom();
+        }
+    }
   return (
     <div className="homePageWrapper">
         <div className="formWrapper">
             <img className='homePageLogo' src="/code-sync.png" alt="editor-logo" />
             <h4 className="mainLabel">Paste Invitation Room Id</h4>
             <div className="inputGroup">
-                <input type="text" className='inputBox' placeholder='ROOM ID' onChange={(e) => setRoomId(e.target.value)} value={roomId} />
-                <input type="text" className='inputBox' placeholder='USERNAME' onChange={(e) => setUsername(e.target.value)} value={username}/>
+                <input type="text" className='inputBox' placeholder='ROOM ID' onChange={(e) => setRoomId(e.target.value)} value={roomId} onKeyUp={handleInputEnter}/>
+                <input type="text" className='inputBox' placeholder='USERNAME' onChange={(e) => setUsername(e.target.value)} value={username} onKeyUp={handleInputEnter}/>
                 <button className='btn joinBtn' onClick={joinRoom}>Join Now</button>
                 <span className='createInfo'>Don't have an invite? 
                 <a onClick={createNewRoom} href="" className='createNewBtn'>Create Room</a>
