cc26c82f9aebbeb5a174f033c33e87528d06fecd
Madhav007 <madhavaprashath669@gmail.com>
2023-06-13 20:15:24 +0530

    Set Roomid/username using uuid

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,17 +1,27 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'; 
 function Home() {
+
+    const [roomId, setRoomId] = useState('')
+    const [username, setUsername] = useState('')
+    const createNewRoom = (event) => {
+        event.preventDefault();
+        const id = uuidv4();
+        setRoomId(id)
+        
+
+    }
   return (
     <div className="homePageWrapper">
         <div className="formWrapper">
             <img className='homePageLogo' src="/code-sync.png" alt="editor-logo" />
             <h4 className="mainLabel">Paste Invitation Room Id</h4>
             <div className="inputGroup">
-                <input type="text" className='inputBox' placeholder='ROOM ID' />
-                <input type="text" className='inputBox' placeholder='USERNAME' />
+                <input type="text" className='inputBox' placeholder='ROOM ID' onChange={(e) => setRoomId(e.target.value)} value={roomId} />
+                <input type="text" className='inputBox' placeholder='USERNAME' onChange={(e) => setUsername(e.target.value)} value={username}/>
                 <button className='btn joinBtn'>Join Now</button>
                 <span className='createInfo'>Don't have an invite? 
-                <a href="" className='createNewBtn'>Create Room</a>
+                <a onClick={createNewRoom} href="" className='createNewBtn'>Create Room</a>
                 </span>
             </div>
         </div>
