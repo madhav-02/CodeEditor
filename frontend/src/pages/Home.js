70f0cf5fffd5ba3a0ed6e88a5f554ed347578d1d
Madhav007 <madhavaprashath669@gmail.com>
2023-06-13 20:04:53 +0530

    Add home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home page</div>
+    <div className="homePageWrapper">
+        <div className="formWrapper">
+            <img className='homePageLogo' src="/code-sync.png" alt="editor-logo" />
+            <h4 className="mainLabel">Paste Invitation Room Id</h4>
+            <div className="inputGroup">
+                <input type="text" className='inputBox' placeholder='ROOM ID' />
+                <input type="text" className='inputBox' placeholder='USERNAME' />
+                <button className='btn joinBtn'>Join Now</button>
+                <span className='createInfo'>Don't have an invite? 
+                <a href="" className='createNewBtn'>Create Room</a>
+                </span>
+            </div>
+        </div>
+        <footer>
+            <h4>Built with ❤</h4>
+        </footer>
+    </div>
   )
 }
 
